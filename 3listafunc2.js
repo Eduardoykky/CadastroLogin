e6f55c97c1571cec7c29c0657423bcf806325687
Eduardo Oliveira <eduardoykky@gmail.com>
2022-11-03 00:08:35 -0300

    primeiro commit

---
+++ 3listafunc2.js
@@ -0,0 +1,10 @@
+
+var nomes = []
+var senhas = []
+var index = 0
+
+function CadastrarUsuario(){
+    nomes[index] = prompt("Insira seu nome")
+    senhas[index] = prompt("Insira sua senha")
+    index++
+}
